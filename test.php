88c3ed68bc8b3bd91693ee06704d3442498da5f1
lahiru04 <prasannalahiru@gmail.com>
2021-07-01 15:13:07 +0530

    Create test.php
    edit

---
+++ test.php
@@ -0,0 +1,29 @@
+{
+  "result": true,
+  "product_price": [
+    {
+      "item_id": 1,
+      "catalog_no": "12311708",
+      "pri_code_dss": "AAR-DEL001",
+      "dss_p_without_tax": "760.00",
+      "dss_vat_amt": "20.00",
+      "dss_p_include_tax": "780.00",
+      "pri_code_rto": "DSS-RTO009",
+      "rto_p_without_tax": "450.00",
+      "rto_vat_amt": "20.00",
+      "rto_p_include_tax": "470.00"
+    },
+    {
+      "item_id": 2,
+      "catalog_no": "12311709",
+      "pri_code_dss": "AAR-DEL002",
+      "dss_p_without_tax": "780.00",
+      "dss_vat_amt": "20.00",
+      "dss_p_include_tax": "800.00",
+      "pri_code_rto": "DSS-RTO010",
+      "rto_p_without_tax": "470.00",
+      "rto_vat_amt": "20.00",
+      "rto_p_include_tax": "490.00"
+    }
+  ]
+}
